f2c386191c1663aa0d2787d8f193c9de8884b8a3
= <joao.souza@avenuecode.com>
2021-09-29 09:59:03 -0300

    component item country

---
+++ src/components/ItemCountry.vue
@@ -0,0 +1,25 @@
+<template>
+  <a href="/">
+    <section>
+      <div>
+        <img :src=flag alt="flag pais" />
+      </div>
+      <div>
+        <h1>{{commonName}}</h1>
+        <p><b>Population:</b>{{population}}</p>
+        <p><b>Region:</b>{{region}}</p>
+        <p><b>Capital:</b>{{capital}}</p>
+      </div>
+    </section>
+  </a>
+</template>
+
+<script>
+export default {
+  name: "itemCountry",
+  props: ["commonName", "flag", "capital", "region", "population"],
+};
+</script>
+
+<style>
+</style>
