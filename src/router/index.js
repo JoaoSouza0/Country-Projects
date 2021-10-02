0815b671eaaa64cc0c8d1c190b9393e164ed3acb
= <joao.souza@avenuecode.com>
2021-10-02 20:16:26 -0300

    fixing style file

--- src/router/index.js
+++ src/router/index.js
@@ -4,13 +4,11 @@ import Home from '../views/Home.vue'
 
 Vue.use(VueRouter)
 
-const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-]
+const routes = [{
+  path: '/',
+  name: 'Home',
+  component: Home
+}, ]
 
 const router = new VueRouter({
   mode: 'history',
