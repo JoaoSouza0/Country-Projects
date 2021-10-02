f8687d6ea83ec1dfc10228c56ab394b042c8f257
= <joao.souza@avenuecode.com>
2021-10-02 20:17:05 -0300

    fixing tag name error

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,21 @@
 <template>
-  <div id="app">
-    <Header />
-    <router-view />
-  </div>
+    <div id="app">
+        <header-component />
+        <router-view />
+    </div>
 </template>
 
 <script>
-import Header from "@/components/Header.vue";
+import HeaderComponent from '@/components/Header.vue';
 export default {
-  components: {
-    Header,
-  },
+    components: {
+        HeaderComponent
+    }
 };
 </script>
 
 <style>
-@import url('https://fonts.googleapis.com/css2?family=Nunito+Sans:wght@600;700;800&display=swap');
+@import url('https://fonts.googleapis.com/css2?family=Nunito+Sans:wght@300;600;700;800&display=swap');
 
 body,
 ul,
@@ -23,25 +23,23 @@ li,
 h1,
 h2,
 p {
-  padding: 0px;
-  margin: 0px;
+    padding: 0px;
+    margin: 0px;
 }
 ul {
-  list-style: none;
+    list-style: none;
 }
 img {
-  max-width: 100%;
-  display: block;
+    width: 100%;
+    display: block;
 }
 :root {
-  --very-dark-blue: hsl(207, 26%, 17%);
-  --dark-blue: hsl(209, 23%, 22%);
+    --very-dark-blue: hsl(207, 26%, 17%);
+    --dark-blue: hsl(209, 23%, 22%);
 }
 
 body {
-  background-color: var(--very-dark-blue);
-  font-family: "Nunito Sans", sans-serif;
-
-
+    background-color: var(--very-dark-blue);
+    font-family: 'Nunito Sans', sans-serif;
 }
 </style>
