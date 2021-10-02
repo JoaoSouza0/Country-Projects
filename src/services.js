e9403d1a88d7064a56014b4a349d9d16594dedcc
= <joao.souza@avenuecode.com>
2021-10-02 20:17:37 -0300

    fixing style service file

--- src/services.js
+++ src/services.js
@@ -1,7 +1,7 @@
 export const api = {
 
     async getCountry(endpoint) {
-       const data  = await (await fetch(`https://restcountries.com/v2${endpoint}`)).json()
-       return data
+        const data = await (await fetch(`https://restcountries.com/v2${endpoint}`)).json()
+        return data
     }
 }
